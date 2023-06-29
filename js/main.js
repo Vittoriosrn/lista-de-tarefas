4f74d4269abd2aa3e281311f6eedd20398958991
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2023-06-29 11:49:10 -0300

    Atualizacao lista de tarefas

--- js/main.js
+++ js/main.js
@@ -4,9 +4,13 @@ const todo_input = document.querySelector('#todo_input')
 const todo_list = document.querySelector('#todo_list')
 const edit_form = document.querySelector('#edit_form')
 const edit_input = document.querySelector('#edit_input')
-const cancel_edit_btn = document.querySelector('#cancel_edit_btn')
+const filter_select = document.querySelector('#filter_select')
+const cancel_edit_btn = document.querySelector('.cancel_edit_btn')
 
-// Funções
+let old_input_value
+
+//------------------ Funções -----------------------//
+
 const save_todo = (text) =>{
     const todo = document.createElement("div")
     todo.classList.add("todo") // Adiciona classe "todo" à div
@@ -32,13 +36,32 @@ const save_todo = (text) =>{
 
     todo_list.appendChild(todo)
     todo_input.value = "" //Limpa o input apos enviar o titulo da lista
-    todo_input.focus()
+    todo_input.focus() // Seleciona o input novamento após o envio
 } 
 
-// Eventos
-todo_form.addEventListener("submit", (e) => {
+const toggleForm = () =>{ // Função para mostrar a edição
+    edit_form.classList.toggle("hide")  // toggle serve para trocar adicionar class ou remover
+    todo_form.classList.toggle("hide")
+    todo_list.classList.toggle("hide")
+}
+
+const uptdateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo") // Seleciona todos os todos/tarefas
+
+    todos.forEach((todo) => {
+        let todo_title = todo.querySelector("h3")
+
+        if(todo_title.innerText === old_input_value){
+            todo_title.innerText = text
+        }
+    })
+
+}
+
+//------------------- Eventos -----------------------//
+
+todo_form.addEventListener("submit", (e) => {  // Adicionar nova tarefa
     e.preventDefault() // Fez com que o evento (e) seja cancelado
-    console.log("Enviou form")
     // ^^^^  Faz com que envie o formulário sem recarregar a página
 
     const input_value = todo_input.value
@@ -47,12 +70,69 @@ todo_form.addEventListener("submit", (e) => {
     }
 })
 
-document.addEventListener("click", (e) => {
+document.addEventListener("click", (e) => {     // Detectar eventos de cliques nos botões
     const target_element = e.target  // Aqui identificamos o item clicado
     const parent_element = target_element.closest("div") // selecionei o elemento pai mais proximo
+    let todo_title;
 
+    if(parent_element && parent_element.querySelector("h3")){           // Verifica se contem um texto no parent_element
+        todo_title = parent_element.querySelector("h3").innerText       // todo_title recebe um texto no parent_element
+    }
+    // Finalizar tarefa
     if(target_element.classList.contains("finish_todo")){
         console.log("Clicou para finalizar")
         parent_element.classList.toggle("done") // se tiver done tira, se não tiver coloca
     }
+    // Remover tarefa
+    if(target_element.classList.contains("remove_todo")){
+        console.log("Tarefa removida")
+        parent_element.remove() // Remover elemento pai
+    }
+    // Mostrar painel de edição da tarefa
+    if(target_element.classList.contains("edit_todo")){
+        toggleForm() // Chama função para mostrar a edição
+
+        edit_input.value = todo_title
+        old_input_value = todo_title
+    }
 })
+
+cancel_edit_btn.addEventListener("click", (e) => {  // Clicou no botao cancelar edição
+    e.preventDefault() //  Não enviar formulário
+
+    toggleForm()
+})
+
+edit_form.addEventListener("submit", (e) => { // Editar tafera ao clicar no botão de enviar
+    e.preventDefault()
+
+    const edit_input_value = edit_input.value
+
+    if(edit_input_value){
+        uptdateTodo(edit_input_value)
+    }
+    toggleForm()
+})
+
+filter_select.addEventListener("change", (e) => {
+    const filtro_selected = e.target.value  // Pega o valor selecionado do select
+    const todo = document.querySelector(".todo")
+    console.log(filtro_selected)
+
+
+    switch (filtro_selected) { 
+        case "all":
+            
+        break
+        
+        case "done":
+            todo.classList.toggle("hide")
+            console.log(todo)
+        break
+
+        case "todo":
+            todo.classList.toggle("hide")
+    }
+})
+
+
